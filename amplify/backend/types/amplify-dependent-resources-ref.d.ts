b8fd8177949738ba9a93c709617a86147053e133
colin mcgovern <colinmcg@udel.edu>
2023-06-28 17:29:16 -0400

    working on adding todo

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "learningappsync": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,10 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import { Amplify } from 'aws-amplify'
+import amplify from '../aws-exports'
+Amplify.configure(amplify)
+
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }

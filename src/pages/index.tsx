469d729e8431b87b5ab50a365d4f1ddaac6bdd4e
colin mcgovern <colinmcg@udel.edu>
2023-06-29 14:07:33 -0400

    trying to get createTodo to work

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,81 +1,98 @@
 import Head from "next/head";
 import Image from "next/image";
 
-import { API } from "aws-amplify"
+import { API, graphqlOperation } from "aws-amplify"
 import { listTodos } from "../graphql/queries"
 
-import React, { useEffect } from "react"
-import {
-  Button,
-  Flex,
-  Heading,
-  Text,
-  TextField,
-  View,
-  withAuthenticator,
-} from "@aws-amplify/ui-react";
+import React, { useState, useEffect } from "react"
+import TextField from '@mui/material/TextField';
+import Button from "@mui/material/Button"
+import Stack from "@mui/material/Stack"
 
 export default function List() {
 
-  useEffect(() => {
-    fetchTodos();
-  }, []);
-  
-  async function fetchTodos(){
-    const data = await API.graphql({ query: listTodos });
-    const notesFromAPI = data.listTodos
-    console.log(notesFromAPI)
-  }
+  const [formData, setFormData] = useState({id: '', name: '', body: ''});
 
-  async function createTodo(){
+  // useEffect(() => {
+  //   fetchTodos();
+  // }, []);
+  
+  // async function fetchTodos(){
+  //   const data = await API.graphql({ query: listTodos });
+  //   const notesFromAPI = data.listTodos
+  //   console.log(notesFromAPI)
+  // }
+
+  async function createTodo(event){
     event.preventDefault();
     const form = new FormData(event.target)
     const data = {
-      id: form.get("id"),
-      name: form.get("name"),
-      body: form.get("body"),
+      id: formData["id"],
+      name: formData["name"],
+      body: formData["body"],
       completed: false
     }
-    await API.graphql({
-      query: createTodo,
-      variables: { input: data },
-    });
-    fetchNotes();
+    await API.graphql<GraphQLQuery<CreateTodoMutation>>(
+      graphqlOperation(
+        createTodo,
+        {input: data})
+     );
+    {/*fetchNotes();*/}
     event.target.reset();
   }
 
+  const onInputChange = (a, b) => {
+    let key = "";
+    let value = "";
+
+    if (typeof b !== "undefined") {
+      key = a;
+      value = b;
+    } else {
+      key = event.target["id"];
+      value = event.target["value"];
+    }
+
+    let new_formData = formData;
+    new_formData[key] = value;
+    setFormData(new_formData);
+  };
+
   return(
     <>
-      <View as="form" margin="3rem 0" onSubmit={createTodo}>
-        <Flex direction="row" justifyContent="center">
+      <form onSubmit={createTodo}>
+          <Stack direction="column" sx={{ width: "50%", margin: "auto" }}>
           <TextField
-            name="id"
-            placeholder="id"
+            required
+            id="id"
             label="id"
-            labelHidden
-            variation="quiet"
-            required
+            margin="normal"
+            onChange={(event) =>
+              onInputChange(event.target["id"], event.target["value"])
+            }
           />
           <TextField
-            name="name"
-            placeholder="name"
+            required
+            id="name"
             label="name"
-            labelHidden
-            variation="quiet"
-            required
+            margin="normal"
+            onChange={(event) =>
+              onInputChange(event.target["name"], event.target["value"])
+            }
           />
           <TextField
-            name="body"
-            placeholder="body"
-            label="body"
-            labelHidden
-            variation="quiet"
             required
+            id="body"
+            label="body"
+            margin="normal"
+            onChange={(event) =>
+              onInputChange(event.target["body"], event.target["value"])
+            }
           />
-          <Button type="submit" variation="primary">
+          <Button type="submit" variation="contained">
             Create
           </Button>
-        </Flex>
-      </View>
+        </Stack>
+      </form>
     </>);
 }
